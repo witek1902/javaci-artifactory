f00fce545941994b3a08c2ffa6f109209be97ef8
Robert Kuśmierek <robert.kusmierek@altkom.pl>
2019-04-24 14:23:21 +0200

    remove magic numbers (uids)

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ docker run \
   -d \
   --restart=unless-stopped \
   -p 8081:8081 \
-  -u 997:993 \
+  -u $(id -u artifactory):$(id -g artifactory) \
   -v /opt/artifactory:/var/opt/jfrog/artifactory \
   -e EXTRA_JAVA_OPTIONS='-Xms512m -Xmx2g -Xss256k -XX:+UseG1GC' \
   docker.bintray.io/jfrog/artifactory-oss
--- setup.sh
+++ setup.sh
@@ -1,2 +1,3 @@
+adduser artifactory
 firewall-cmd --permanent --add-port=8081/tcp
 firewall-cmd --reload
